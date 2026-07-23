0dbba0fe4ee3662e13db71741158978b1902c4a7
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-23 10:50:00 +0530

    Added authentification

--- myApp/app/drawer/(tabs)/login.jsx
+++ myApp/app/drawer/(tabs)/login.jsx
@@ -1,6 +1,7 @@
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native'
 import React, {useState} from 'react'
 import * as SecureStore from 'expo-secure-store'
+import * as LocalAuthentication from 'expo-local-authentication'
 import {router} from 'expo-router'
 
 const Login = () => {
@@ -10,7 +11,44 @@ const Login = () => {
     const handleLogin = async () => {
 
         if(name==="Zeel" && password==="1234567"){
-            await SecureStore.setItemAsync("India","Bharat45")
+            await SecureStore.setItemAsync("token","Bharat45")
+            await SecureStore.setItemAsync("biometric","true")
+            router.replace("/home")
+        }
+        else{
+            alert("Invalid Credentials")
+        }
+
+    }
+
+
+    const handleBiometric = async() => {
+
+        const token = await SecureStore.getItemAsync("token")
+        const biometric = await SecureStore.getItemAsync("biometric")
+
+        if(!token || biometric!=="true"){
+            alert("login with username and password")
+            return
+        }
+
+        const hasHardWare = await LocalAuthentication.hasHardwareAsync()
+
+        if(!hasHardWare){
+            alert("Biometric does not support")
+            return
+        }
+
+        const isEnrolled = await LocalAuthentication.isEnrolledAsync()
+
+        if(!isEnrolled){
+            alert("Fingerprint and facelock is not required")
+            return
+        }
+
+        const result = await LocalAuthentication.authenticateAsync()
+
+        if(result.success){
             router.replace("/home")
         }
         else{
@@ -23,10 +61,13 @@ const Login = () => {
         <View style={styles.container}>
             <Text style={styles.title}>Login</Text>
 
-            <TextInput style={styles.input}placeholder="Enter name..." value={name}  onChangeText={setName} />
+            <TextInput style={styles.input}placeholder="Enter name..." value={name} onChangeText={setName} />
 
             <TextInput style={styles.input} placeholder="Enter password..." value={password} onChangeText={setPassword} secureTextEntry />
+            
             <Button title="Login" onPress={handleLogin} />
+
+            <Button title="Login with Biometric" onPress={handleBiometric} />
         </View>
     )
 }
