6fd1dfbbe87e9b764ffd16284bc014eb518db471
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-27 14:24:31 +0530

    added camera zoom in and slider features

--- myApp/app/drawer/(tabs)/newcamera.jsx
+++ myApp/app/drawer/(tabs)/newcamera.jsx
@@ -1,10 +1,12 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import {Text, StyleSheet, View, Button} from "react-native";
-import {CameraView, useCameraPermissions} from "expo-camera"
+import {CameraView, useCameraPermissions} from "expo-camera";
+import Slider from "@react-native-community/slider";
 
 export default function CameraScreen(){
     const [permission, requestPermission] = useCameraPermissions();
     const cameraRef = useRef(null);
+    const [zoom, setZoom] = useState(0);
 
     if(!permission?.granted){
         return(
@@ -17,7 +19,26 @@ export default function CameraScreen(){
     return(
         <View style={style.container}>
             <Text> Camera Demo Start</Text>
-            <CameraView style={style.camera} facing="back" ref={cameraRef}/>
+
+            <CameraView 
+                style={style.camera} 
+                facing="back" 
+                ref={cameraRef}
+                zoom={zoom}
+            />
+
+            <Text style={style.zoomText}>
+                Zoom: {Math.round(zoom * 100)}%
+            </Text>
+
+            <Slider
+                style={style.slider}
+                minimumValue={0}
+                maximumValue={1}
+                value={zoom}
+                onValueChange={setZoom}
+            />
+
         </View>
     );
 }
@@ -33,5 +54,16 @@ const style = StyleSheet.create({
         width:"100%",
         height:500,
         marginTop:20
+    },
+
+    zoomText:{
+        fontSize:18,
+        textAlign:"center",
+        marginTop:20
+    },
+
+    slider:{
+        width:"100%",
+        height:40
     }
 })
