4f6639992b4f92b841a117627cc82129602d841a
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-27 14:27:20 +0530

    added camera front and back features

--- myApp/app/drawer/(tabs)/newcamera.jsx
+++ myApp/app/drawer/(tabs)/newcamera.jsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import {Text, StyleSheet, View, Button} from "react-native";
+import {Text, StyleSheet, View, Button, TouchableOpacity} from "react-native";
 import {CameraView, useCameraPermissions} from "expo-camera";
 import Slider from "@react-native-community/slider";
 
@@ -7,6 +7,15 @@ export default function CameraScreen(){
     const [permission, requestPermission] = useCameraPermissions();
     const cameraRef = useRef(null);
     const [zoom, setZoom] = useState(0);
+    const [facing, setFacing] = useState("back");
+
+    const changeCamera = () => {
+        if(facing === "back"){
+            setFacing("front");
+        }else{
+            setFacing("back");
+        }
+    }
 
     if(!permission?.granted){
         return(
@@ -18,11 +27,11 @@ export default function CameraScreen(){
 
     return(
         <View style={style.container}>
-            <Text> Camera Demo Start</Text>
+            <Text style={style.title}> Camera Demo Start</Text>
 
             <CameraView 
                 style={style.camera} 
-                facing="back" 
+                facing={facing}
                 ref={cameraRef}
                 zoom={zoom}
             />
@@ -39,6 +48,10 @@ export default function CameraScreen(){
                 onValueChange={setZoom}
             />
 
+            <TouchableOpacity style={style.button} onPress={changeCamera}>
+                <Text style={style.buttonText}>Switch Camera</Text>
+            </TouchableOpacity>
+
         </View>
     );
 }
@@ -50,20 +63,42 @@ const style = StyleSheet.create({
         backgroundColor:"white"
     },
 
+    title:{
+        fontSize:20,
+        fontWeight:"bold",
+        textAlign:"center"
+    },
+
     camera:{
         width:"100%",
-        height:500,
+        height:400,
         marginTop:20
     },
 
     zoomText:{
         fontSize:18,
         textAlign:"center",
-        marginTop:20
+        marginTop:15
     },
 
     slider:{
         width:"100%",
         height:40
+    },
+
+    button:{
+        width:"100%",
+        height:50,
+        backgroundColor:"black",
+        justifyContent:"center",
+        alignItems:"center",
+        borderRadius:10,
+        marginTop:10
+    },
+
+    buttonText:{
+        color:"white",
+        fontSize:16,
+        fontWeight:"bold"
     }
 })
