3f27d6164a84276541c16b690d097f11f516bb20
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-07-27 13:59:32 +0530

    added new camera with facing=front correct

--- myApp/app/drawer/(tabs)/newcamera.jsx
+++ myApp/app/drawer/(tabs)/newcamera.jsx
@@ -1,69 +1,36 @@
 import { useRef } from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
-import { CameraView, useCameraPermissions } from "expo-camera";
+import {Text, StyleSheet, View, Button} from "react-native";
+import {CameraView, useCameraPermission} from "expo-camera"
 
-export default function CameraScreen() {
-  const [permission, requestPermission] = useCameraPermissions();
-  const cameraRef = useRef(null);
+export default function CameraScreen(){
+    const [permission, requestPermission] = useCameraPermission();
+    const cameraRef = useRef(null);
 
-  if (!permission?.granted) {
-    return (
-      <View style={styles.permissionContainer}>
-        <Text style={styles.permissionText}>
-          Camera permission is required
-        </Text>
-
-        <Button
-          title="Grant Permission"
-          onPress={requestPermission}
-        />
-      </View>
+    if(!permission?.granted){
+        return(
+            <View>
+                <Button title="Grant Permission" onPress={requestPermission}/>
+            </View>
+        );
+    }
+    return(
+        <View style={style.container}>
+            <Text> Camera Demo Start</Text>
+            <CameraView style={style.camera} facing="front" ref={cameraRef}/>
+        </View>
     );
-  }
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Camera Demo Start</Text>
-
-      <CameraView
-        style={styles.camera}
-        facing="front"
-        ref={cameraRef}
-      />
-    </View>
-  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: "#ffffff",
-  },
+const style = StyleSheet.create({
+    container:{
+        flex:1,
+        padding:20,
+        backgroundColor:"white"
+    },
 
-  title: {
-    fontSize: 22,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 20,
-  },
-
-  camera: {
-    width: "100%",
-    height: 450,
-    borderRadius: 15,
-  },
-
-  permissionContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
-  },
-
-  permissionText: {
-    fontSize: 18,
-    marginBottom: 20,
-    textAlign: "center",
-  },
-});
+    camera:{
+        width:"100%",
+        height:500,
+        marginTop:20
+    }
+})
