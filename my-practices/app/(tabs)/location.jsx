73d7d27584f844b121f5029099eed454c8405923
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-08-03 09:33:36 +0530

    Added basic file structure

---
+++ my-practices/app/(tabs)/location.jsx
@@ -0,0 +1,13 @@
+import {View, Button,StyleSheet} from "react-native";
+
+export default function LocationScreen(){
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    
+})
