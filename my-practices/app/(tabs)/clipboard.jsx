e54d372996a6ec3bbef471b302f2f3c2133ab442
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-08-21 07:02:18 +0530

    added clipboard code

--- my-practices/app/(tabs)/clipboard.jsx
+++ my-practices/app/(tabs)/clipboard.jsx
@@ -1,12 +1,83 @@
+// import React, { useState } from "react";
+// import {
+//   View,
+//   Text,
+//   Button,
+//   Image,
+//   StyleSheet,
+//   Alert,
+// } from "react-native";
+// import * as Clipboard from "expo-clipboard";
+// import * as FileSystem from "expo-file-system/legacy";
+
+// export default function App() {
+//   const [image, setImage] = useState(null);
+
+//   const getImage = async () => {
+   
+//       const res = await Clipboard.getImageAsync({
+//         format: "png",
+//       });
+
+//       setImage(res?.data)
+
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>
+//         Clipboard Image
+//       </Text>
+
+//       <Button
+//         title="Get Clipboard Image"
+//         onPress={getImage}
+//       />
+
+//       <View style={styles.imageBox}>
+//         {
+//             image && (
+//                 <Image source={{uri:image}} style={{height:100, width:100}} />
+//             )
+//         }
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     padding: 20,
+//     backgroundColor:"teal"
+//   },
+
+//   title: {
+//     fontSize: 24,
+//     marginBottom: 30,
+//   },
+
+//   imageBox: {
+//     width: 320,
+//     height: 320,
+//     marginTop: 30,
+//     borderWidth: 2,
+//     borderColor: "white",
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+
+//   image: {
+//     width: 300,
+//     height: 300,
+//   },
+// });
+
+
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  Button,
-  Image,
-  StyleSheet,
-  Alert,
-} from "react-native";
+import { View, Text, Button, Image, StyleSheet, Alert,} from "react-native";
 import * as Clipboard from "expo-clipboard";
 import * as FileSystem from "expo-file-system/legacy";
 
@@ -18,7 +89,7 @@ export default function App() {
       const res = await Clipboard.getImageAsync({
         format: "png",
       });
-
+console.log(res)
       setImage(res?.data)
 
   }
@@ -51,7 +122,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
-    backgroundColor:"teal"
+    backgroundColor:"lightblue"
   },
 
   title: {
@@ -64,7 +135,6 @@ const styles = StyleSheet.create({
     height: 320,
     marginTop: 30,
     borderWidth: 2,
-    borderColor: "white",
     justifyContent: "center",
     alignItems: "center",
   },
