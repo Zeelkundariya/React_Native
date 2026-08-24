7766002cc1841107e6b8bc7f38be5e2b41180fd7
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-08-24 09:32:54 +0530

    added share code

---
+++ my-practices/app/(tabs)/share.jsx
@@ -0,0 +1,23 @@
+import { View, Text , Button, Share, } from "react-native";
+
+export default function ShareScreen() {
+
+    const handleShare = async () => {
+        await Share.share({ message: "Hello I am Zeel" })
+    }
+    return (
+        <View style={styles.container}>
+            <Text>Share Class</Text>
+            <Button title="Share Text" onPress={handleShare} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+});
