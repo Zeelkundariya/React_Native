fbdf30e8b41d6d861e8b0bc11fd198b8e838e877
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-08-31 17:57:59 +0530

    Added easy  storage code

--- my-practices/app/(tabs)/storage.jsx
+++ my-practices/app/(tabs)/storage.jsx
@@ -1,78 +1,91 @@
-import React, { useEffect, useState } from "react";
 import { View, Text, TextInput, Button } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useState } from "react";
 
-const AsyncStorageScreen = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+export default function AsyncStorageScreen() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-    const [saveData, setSaveData] = useState("")
-    const handleSaveData = async () => {
-        try {
-            const user = {
-                userName: name,
-                userEmail: email,
-                userPassword: password,
-            };
-
-            await AsyncStorage.setItem("userData", JSON.stringify(user));
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    const handleGetData = async () => {
-        try {
-            const value = await AsyncStorage.getItem("userData");
-
-            if (value !== null) {
-                const user = JSON.parse(value);
-
-                setName(user.userName);
-                setEmail(user.userEmail);
-                setPassword(user.userPassword);
-
-                setSaveData(user.userName);
-            }
-        } catch (error) {
-            console.log(error);
-        }
+  // Save
+  const saveData = async () => {
+    const user = {
+      name: name,
+      email: email,
     };
 
-    const handleRemoveData = async () => {
-        try {
-            await AsyncStorage.removeItem("userData")
-            setName("");
-            setEmail("");
-            setPassword("");
-            setSaveData("");
-        } catch (error) {
-            console.log(error)
-        }
+    await AsyncStorage.setItem("user", JSON.stringify(user));
+
+    console.log("Data Saved");
+  };
+
+  // Get
+  const getData = async () => {
+    const data = await AsyncStorage.getItem("user");
+
+    if (data) {
+      const user = JSON.parse(data);
+
+      setName(user.name);
+      setEmail(user.email);
     }
-    useEffect(() => {
-        handleGetData()
-    }, [])
+  };
 
-    return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 20 }}>
-            <Text style={{ fontSize: 22, fontWeight: "bold" }}>AsyncStorage Demo Class</Text>
-            <TextInput placeholder="Enter Name.." value={name} onChangeText={setName} style={{ borderWidth: 1, marginBottom: 20, padding: 15 }} />
-            <TextInput placeholder="Enter Email.." value={email} onChangeText={setEmail} style={{ borderWidth: 1, marginBottom: 20, padding: 15 }} />
-            <TextInput placeholder="Enter Password.." value={password} onChangeText={setPassword} style={{ borderWidth: 1, marginBottom: 20, padding: 15 }} />
-            <View style={{ height: 20 }} />
+  // Delete
+  const deleteData = async () => {
+    await AsyncStorage.removeItem("user");
 
-            <Button title="Save Data" onPress={handleSaveData} />
-            <View style={{ height: 20 }} />
-            <Button title="Get Data" onPress={handleGetData} />
-            
-            <Text>Name: {name}</Text>
-            <Text>Email: {email}</Text>
-            <Text>Password: {password}</Text>
+    setName("");
+    setEmail("");
 
-            <View style={{ height: 20 }} />
-            <Button title="Delete Data" onPress={handleRemoveData} />
-        </View>
-    )
+    console.log("Data Deleted");
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        padding: 20,
+      }}
+    >
+      <Text>Name</Text>
+
+      <TextInput
+        placeholder="Enter name"
+        value={name}
+        onChangeText={setName}
+        style={{
+          borderWidth: 1,
+          padding: 10,
+          marginBottom: 10,
+        }}
+      />
+
+      <Text>Email</Text>
+
+      <TextInput
+        placeholder="Enter email"
+        value={email}
+        onChangeText={setEmail}
+        style={{
+          borderWidth: 1,
+          padding: 10,
+          marginBottom: 20,
+        }}
+      />
+
+      <Button title="Save Data" onPress={saveData} />
+
+      <View style={{ height: 10 }} />
+
+      <Button title="Get Data" onPress={getData} />
+
+      <View style={{ height: 10 }} />
+
+      <Button title="Delete Data" onPress={deleteData} />
+
+      <Text>Name: {name}</Text>
+      <Text>Email: {email}</Text>
+    </View>
+  );
 }
-export default AsyncStorageScreen
