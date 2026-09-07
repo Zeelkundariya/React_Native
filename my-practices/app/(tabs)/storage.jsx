9bce21306c7dd8f082f7dd54e208af2734444937
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-09-07 09:14:10 +0530

    added storage code

--- my-practices/app/(tabs)/storage.jsx
+++ my-practices/app/(tabs)/storage.jsx
@@ -1,94 +1,94 @@
-// import { View, Text, TextInput, Button } from "react-native";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
-// import { useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useState } from "react";
 
-// export default function AsyncStorageScreen() {
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
+export default function AsyncStorageScreen() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-//   // Save
-//   const saveData = async () => {
-//     const user = {
-//       name: name,
-//       email: email,
-//     };
+  // Save
+  const saveData = async () => {
+    const user = {
+      name: name,
+      email: email,
+    };
 
-//     await AsyncStorage.setItem("user", JSON.stringify(user));
+    await AsyncStorage.setItem("user", JSON.stringify(user));
 
-//     console.log("Data Saved");
-//   };
+    console.log("Data Saved");
+  };
 
-//   // Get
-//   const getData = async () => {
-//     const data = await AsyncStorage.getItem("user");
+  // Get
+  const getData = async () => {
+    const data = await AsyncStorage.getItem("user");
 
-//     if (data) {
-//       const user = JSON.parse(data);
+    if (data) {
+      const user = JSON.parse(data);
 
-//       setName(user.name);
-//       setEmail(user.email);
-//     }
-//   };
+      setName(user.name);
+      setEmail(user.email);
+    }
+  };
 
-//   // Delete
-//   const deleteData = async () => {
-//     await AsyncStorage.removeItem("user");
+  // Delete
+  const deleteData = async () => {
+    await AsyncStorage.removeItem("user");
 
-//     setName("");
-//     setEmail("");
+    setName("");
+    setEmail("");
 
-//     console.log("Data Deleted");
-//   };
+    console.log("Data Deleted");
+  };
 
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         padding: 20,
-//       }}
-//     >
-//       <Text>Name</Text>
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        padding: 20,
+      }}
+    >
+      <Text>Name</Text>
 
-//       <TextInput
-//         placeholder="Enter name"
-//         value={name}
-//         onChangeText={setName}
-//         style={{
-//           borderWidth: 1,
-//           padding: 10,
-//           marginBottom: 10,
-//         }}
-//       />
+      <TextInput
+        placeholder="Enter name"
+        value={name}
+        onChangeText={setName}
+        style={{
+          borderWidth: 1,
+          padding: 10,
+          marginBottom: 10,
+        }}
+      />
 
-//       <Text>Email</Text>
+      <Text>Email</Text>
 
-//       <TextInput
-//         placeholder="Enter email"
-//         value={email}
-//         onChangeText={setEmail}
-//         style={{
-//           borderWidth: 1,
-//           padding: 10,
-//           marginBottom: 20,
-//         }}
-//       />
+      <TextInput
+        placeholder="Enter email"
+        value={email}
+        onChangeText={setEmail}
+        style={{
+          borderWidth: 1,
+          padding: 10,
+          marginBottom: 20,
+        }}
+      />
 
-//       <Button title="Save Data" onPress={saveData} />
+      <Button title="Save Data" onPress={saveData} />
 
-//       <View style={{ height: 10 }} />
+      <View style={{ height: 10 }} />
 
-//       <Button title="Get Data" onPress={getData} />
+      <Button title="Get Data" onPress={getData} />
 
-//       <View style={{ height: 10 }} />
+      <View style={{ height: 10 }} />
 
-//       <Button title="Delete Data" onPress={deleteData} />
+      <Button title="Delete Data" onPress={deleteData} />
 
-//       <Text>Name: {name}</Text>
-//       <Text>Email: {email}</Text>
-//     </View>
-//   );
-// }
+      <Text>Name: {name}</Text>
+      <Text>Email: {email}</Text>
+    </View>
+  );
+}
 
 
 
@@ -206,41 +206,66 @@
 
 
 
-import { View } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useState } from "react";
+// import { View } from "react-native";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+// import { useState } from "react";
 
-export default function StorageScreen() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
+// export default function StorageScreen() {
+//     const [name, setName] = useState("");
+//     const [email, setEmail] = useState("");
 
-    const saveData = async () => {
-        const user={
-            name:name,
-            email:email
-        }
-        await AsyncStorage.setItem("user", JSON.stringify(user))
-    }
+//     const saveData = async () => {
+//         const user={
+//             name:name,
+//             email:email
+//         }
+//         await AsyncStorage.setItem("user", JSON.stringify(user))
+//     }
 
-    const getData = async()=>{
-        const data = await AsyncStorage.getItem("user")
+//     const getData = async()=>{
+//         const data = await AsyncStorage.getItem("user")
 
-        if(data){
-            const user = JSON.parse(data);
-            setName(user.name)
-            setEmail(user.email)
-        }
-    }
+//         if(data){
+//             const user = JSON.parse(data);
+//             setName(user.name)
+//             setEmail(user.email)
+//         }
+//     }
 
-    const deleteData = async()=>{
-        await AsyncStorage.removeItem("user")
-        setName("")
-        setEmail("")
+//     const deleteData = async()=>{
+//         await AsyncStorage.removeItem("user")
+//         setName("")
+//         setEmail("")
         
-    }
-    return (
-        <View>
-            <Button title="get" onPress={saveData} />
-        </View>
-    )
-}
+//     }
+//     return (
+//         <View>
+//             <Button title="get" onPress={saveData} />
+//         </View>
+//     )
+// }
+
+// import { View } from "react-native";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+// import { useState } from "react";
+// import { use } from "react";
+
+// export default function AsyncStorageScreen(){
+//     const [name, setName] = useState("");
+//     const [email, setEmail] = useState("")
+
+//     const saveData = async()=>{
+//         const user ={
+//             name:name,
+//             email:email,
+//         }
+
+//         await Notifications.setItem("user" , JSON.stringify(user))
+
+//     }
+//     return(
+//         <View>
+// <Button  title="Save Data" onPress={saveData}/>
+//         </View>
+//     )
+// }
